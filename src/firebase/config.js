7bb626cbfec1b9ebb8fb2ec16a6f2d565aae9c9e
Angie Marquina <angiemarquina65@gmail.com>
2023-02-16 11:20:03 -0500

    US01 completed

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCluTzzNPXlStNE9TfOpccP_qPvGhOQsCA',
+  authDomain: 'mirai-3a75e.firebaseapp.com',
+  projectId: 'mirai-3a75e',
+  storageBucket: 'mirai-3a75e.appspot.com',
+  messagingSenderId: '5447689704',
+  appId: '1:5447689704:web:ed982852402d37e458b05d'
+}
+
+export const app = initializeApp(firebaseConfig)
