361ba7054aa9ccf2657d919e94a8dccd49676b74
Angie Marquina <angiemarquina65@gmail.com>
2023-03-15 21:39:18 -0500

    collection by uid, logout, wall css

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,15 +1,17 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 import { BsXLg } from 'react-icons/bs'
 import { addDoc, collection, updateDoc, doc } from 'firebase/firestore'
 import { db } from '../firebase/config'
 import '../stylesheets/modal.css'
 import { emptyNote } from './Wall'
+import { auth } from '../firebase/auth'
 
 function Modal ({ content, isOpen, onClose, currentNote, setCurrentNote }) {
   const createOrEditNote = async () => {
     if (currentNote.id === '') {
       const date = new Date().toLocaleString()
-      await addDoc(collection(db, 'notes'), {
+      await addDoc(collection(db, `notesfrom${auth.currentUser.uid}`), {
         title: currentNote.title,
         description: currentNote.description,
         date
@@ -17,11 +19,15 @@ function Modal ({ content, isOpen, onClose, currentNote, setCurrentNote }) {
       setCurrentNote(emptyNote)
     } else {
       const date = new Date().toLocaleString()
-      await updateDoc(doc(db, 'notes', currentNote.id), {
-        title: currentNote.title,
-        description: currentNote.description,
-        date
-      })
+      console.log(auth.currentUser.uid)
+      await updateDoc(
+        doc(db, `notesfrom${auth.currentUser.uid}`, currentNote.id),
+        {
+          title: currentNote.title,
+          description: currentNote.description,
+          date
+        }
+      )
       setCurrentNote(emptyNote)
     }
   }
@@ -34,9 +40,9 @@ function Modal ({ content, isOpen, onClose, currentNote, setCurrentNote }) {
   return (
     <>
       {isOpen && (
-        <section className='modal-overlay'>
-          <section className='note-input'>
-            <section className='close-icon'>
+        <section className="modal-overlay">
+          <section className="note-input">
+            <section className="close-icon">
               <BsXLg
                 onClick={() => {
                   onClose()
@@ -44,25 +50,25 @@ function Modal ({ content, isOpen, onClose, currentNote, setCurrentNote }) {
               />
             </section>
             <input
-              className='note-input-title'
-              type='text'
-              placeholder='Title'
-              name='title'
+              className="note-input-title"
+              type="text"
+              placeholder="Title"
+              name="title"
               value={currentNote.title}
               // onChange={(e) => setCurrentNote((prevState) => ({ id: prevState.id, description: prevState.description, title: e.target.value }))}
               // onChange={(e) => setCurrentNote((prevState) => ({ ...prevState, title: e.target.value }))}
               onChange={handleChange}
             />
             <textarea
-              className='note-input-text'
-              placeholder='Start typing'
-              name='description'
+              className="note-input-text"
+              placeholder="Start typing"
+              name="description"
               value={currentNote.description}
               // onChange={(e) => setCurrentNote((prevState) => ({ ...prevState, description: e.target.value }))}
               onChange={handleChange}
             />
             <button
-              className='note-btn'
+              className="note-btn"
               onClick={() => {
                 createOrEditNote()
                 onClose()
